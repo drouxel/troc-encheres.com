ccfcf6b3aa2a0372e07156b6eb377492a9d9d2f0
damien <dam.rouxel@gmail.com>
2020-05-12 14:26:09 +0200

    methode init() enlevée dans ServletConnexion

--- src/fr/eni/trocEncheres/servlets/ServletConnexion.java
+++ src/fr/eni/trocEncheres/servlets/ServletConnexion.java
@@ -12,11 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 public class ServletConnexion extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-     @Override
-    public void init() throws ServletException {
-    	System.out.println("coucou");
-    }  
+	private static final long serialVersionUID = 1L;  
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
