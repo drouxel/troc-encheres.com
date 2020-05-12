ee9e8b34110d12ffa2211f16be9416e8a3385906
Matthieu <matthieu.sourbier@outlook.com>
2020-05-12 14:47:48 +0200

    commit bo

--- src/fr/eni/trocEncheres/bo/Enchere.java
+++ src/fr/eni/trocEncheres/bo/Enchere.java
@@ -1,5 +1,75 @@
 package fr.eni.trocEncheres.bo;
 
+import java.util.Date;
+
 public class Enchere {
+	private Utilisateur encherisseur;
+	private Vente vente;
+	private Date dateEnchere;
 	
+	/**
+	 * Constructeur vide de Enchere
+	 */
+	public Enchere() {
+	}
+	
+	/**
+	 * Constructeur surchargé
+	 * @param encherisseur
+	 * @param vente
+	 * @param dateEnchere
+	 */
+	public Enchere(Utilisateur encherisseur, Vente vente, Date dateEnchere) {
+		this.encherisseur = encherisseur;
+		this.vente = vente;
+		this.dateEnchere = dateEnchere;
+	}
+	
+	/**
+	 * encherisseur
+	 * @return
+	 */
+	public Utilisateur getEncherisseur() {
+		return encherisseur;
+	}
+	
+	/**
+	 * 
+	 * @param encherisseur
+	 */
+	public void setEncherisseur(Utilisateur encherisseur) {
+		this.encherisseur = encherisseur;
+	}
+	
+	/**
+	 * vente
+	 * @return
+	 */
+	public Vente getVente() {
+		return vente;
+	}
+	
+	/**
+	 * 
+	 * @param vente
+	 */
+	public void setVente(Vente vente) {
+		this.vente = vente;
+	}
+	
+	/**
+	 * dateEnchere
+	 * @return
+	 */
+	public Date getDateEnchere() {
+		return dateEnchere;
+	}
+	
+	/**
+	 * 
+	 * @param dateEnchere
+	 */
+	public void setDateEnchere(Date dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
 }
