e5eda26c9abac64958532fa5bbc7c1074369bdb8
Ludo <ludovicd58@gmail.com>
2020-05-19 10:45:19 +0200

    Gestion des erreurs EnchereManager

--- src/fr/eni/trocEncheres/bll/EnchereManager.java
+++ src/fr/eni/trocEncheres/bll/EnchereManager.java
@@ -8,121 +8,147 @@ import fr.eni.trocEncheres.dal.DAOFactory;
 import fr.eni.trocEncheres.dal.EnchereDAO;
 
 /**
- * @author MATTHIEU
- *Ajouter, Supprimer une enchère
+ * @author MATTHIEU Ajouter, Supprimer une enchère
  */
 public class EnchereManager {
 
-	//singleton
-		private static EnchereManager instance;
-		
-		public static EnchereManager getInstance()throws BusinessException {
-			if(instance == null) {
-				instance = new EnchereManager();
-			}
-			return instance;
+	// singleton
+	private static EnchereManager instance;
+
+	public static EnchereManager getInstance() {
+		if (instance == null) {
+			instance = new EnchereManager();
 		}
-	
-	//Attribut(s)
-		EnchereDAO daoEnchere = DAOFactory.getEnchereDAO();
-	
-		/**
-		 * Constructeur par défaut
-		 */
-		public EnchereManager() {
-			daoEnchere = DAOFactory.getEnchereDAO();
-		}
-		
+		return instance;
+	}
+
+	// Attribut(s)
+	EnchereDAO daoEnchere = DAOFactory.getEnchereDAO();
+
+	/**
+	 * Constructeur par défaut
+	 */
+	public EnchereManager() {
+		daoEnchere = DAOFactory.getEnchereDAO();
+	}
+
 	/**
 	 * Methode permettant d'ajouter une enchère
+	 * 
 	 * @throws BusinessException
 	 */
 	public void ajouterEnchere(Enchere e) throws BusinessException {
+		BusinessException BExc = new BusinessException();
 		dateIsValideEnchere(e);
 		montantIsValideEnchere(e);
-		
-		//ajoute une enchère si la date est correcte et si le montant l'est aussi
+
+		// ajoute une enchère si la date est correcte et si le montant l'est aussi
 		try {
-			if (dateIsValideEnchere(e) || montantIsValideEnchere(e)){
-				daoEnchere.ajouterEnchere(e);					
+			if (dateIsValideEnchere(e) || montantIsValideEnchere(e)) {
+				daoEnchere.ajouterEnchere(e);
 			}
 		} catch (BusinessException e2) {
-			throw new BusinessException("Erreur lors de l'ajout de l'enchère.");
+			BExc.ajouterErreur("Erreur lors de l'ajout de l'ench�re.");
 		}
 	}
-	
+
 	/**
 	 * Methode permettant de supprimer une enchère
+	 * 
 	 * @throws BusinessException
 	 */
 	public void supprimerEnchere(Enchere e) throws BusinessException {
+		BusinessException BExc = new BusinessException();
 		try {
 			daoEnchere.supprimerEnchere(e);
 		} catch (BusinessException e1) {
-			throw new BusinessException("Erreur lors de la suppression");
+			BExc.ajouterErreur("Erreur lors de la suppression");
 		}
 	}
-	
+
 	/**
 	 * Methode permettant de supprimer une enchère
+	 * 
 	 * @throws BusinessException
 	 */
 	public void supprimerEnchere(int noVente) throws BusinessException {
+		BusinessException BExc = new BusinessException();
 		try {
 			daoEnchere.supprimerEncheres(noVente);
 		} catch (BusinessException e) {
-			throw new BusinessException("Erreur lors de la suppression");
+			BExc.ajouterErreur("Erreur lors de la suppression");
 		}
 	}
-	
+
 //	public void enchereTerminee()
-	
+
 	/**
 	 * Fonction qui vérifie la date de l'enchère
+	 * 
 	 * @param e
-	 * @return 
+	 * @return
 	 * @throws BusinessException
 	 */
 	private boolean dateIsValideEnchere(Enchere e) throws BusinessException {
+		BusinessException BExc = new BusinessException();
 		// Permet de vérifier le créneau des dates
 		if (e.getDateEnchere().isAfter(e.getVente().getDateFinEnchere())) {
-			throw new BusinessException("Erreur de date");
-		}else {
+			BExc.ajouterErreur("Erreur de date");
+			throw BExc;
+		} else {
 			return true;
 		}
 	}
-	
+
 	/**
 	 * Fonction qui vérifie le montant de l'enchère
+	 * 
 	 * @param e
-	 * @return 
+	 * @return
 	 * @throws BusinessException
 	 */
 	private boolean montantIsValideEnchere(Enchere e) throws BusinessException {
+		BusinessException BExc = new BusinessException();
 		// Si les crésit sont égal à 0, alors pas d'enchère
 		if (e.getCredit() == 0) {
-				throw new BusinessException("Erreur de prix");
-		}else {
+			BExc.ajouterErreur("Erreur de prix");
+			throw BExc;
+		} else {
 			return true;
 		}
 	}
-	
+
 	/**
-	 * Fonction permettant de lister des encheres liées à une vente
-	 * (la liste est triée par ordre décroissant de la meilleure offre à la moins bonne)
+	 * Fonction permettant de lister des encheres liées à une vente (la liste est
+	 * triée par ordre décroissant de la meilleure offre à la moins bonne)
+	 * 
 	 * @param noVente
 	 * @return
 	 * @throws BusinessException
 	 */
 	public List<Enchere> getEncheres(int noVente) throws BusinessException {
+		BusinessException BExc = new BusinessException();
+		try {
+			daoEnchere.getEncheres(noVente);
+		} catch (BusinessException e) {
+			BExc.ajouterErreur("Erreur de r�cup�ration des ench�res li�es � la vente.");
+		}
 		return daoEnchere.getEncheres(noVente);
 	}
-	
-	/**Fonction permettant de lister des encheres
+
+	/**
+	 * Fonction permettant de lister des encheres
+	 * 
 	 * @return
 	 * @throws BusinessException
 	 */
 	public List<Enchere> getEncheres() throws BusinessException {
+		BusinessException BExc = new BusinessException();
+		try {
+			daoEnchere.getEncheres();
+		} catch (BusinessException e) {
+			BExc.ajouterErreur("Erreur de r�cup�ration des ench�res.");
+		}
 		return daoEnchere.getEncheres();
 	}
 }
